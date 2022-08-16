1fbcd7c3ede7e23261bba17be4eb49d1f98ac948
matheusv11 <matheus.silva@icolabora.com>
2022-08-15 22:48:09 -0300

    Revertendo Diva

--- src/utils/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'; // DEIXAR AXIOS GLOBAL
-
-const axiosConfig = axios.create({
-    // baseURL: process.env.API_URL || 'http://localhost:3030'
-    // baseURL: process.env.API_URL || 'http://192.168.1.123:3030'
-    baseURL: "https://yt-music-api.vercel.app"
-});
-
-export default axiosConfig
